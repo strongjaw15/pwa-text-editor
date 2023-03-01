2e7e7b207dd110034a7a9a377d9c0f78a0ce1434
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-02-28 19:39:52 -0600

    Worked on database.js.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,16 +17,16 @@ const initdb = async () =>
 */
 export const putDb = async (content) => {
   // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
-  const DB_VAR = await openDB('DB_NAME', 1);
+  const databaseOpener = await openDB('jate', 1);
 
   // Now create a variable for the transaction
-  const TX_VAR = jateDb.transaction('DB_NAME', 'readwrite');
+  const jateTransaction = jateDb.transaction('jate', 'readwrite');
 
   // Now create a variable for the store
-  const STORE_VAR = tx.objectStore('DB_NAME');
+  const jateStore = tx.objectStore('jate');
 
   // Now create a variable named "request" and have it perform the update
-  const VAR_NAME = store.put({ id: 1, value: content });
+  const jateRequest = store.put({ id: 1, value: content });
 
   const result = await request;
   console.log('🚀 - data saved to the database', result.value);
@@ -39,6 +39,12 @@ export const getDb = async () => {
   // You can duplicate the same first lines of code from above, except that the transaction will be 'readonly'
   
   // LINES 1-3 HERE
+  const databaseOpener = await openDB('jate', 1);
+
+  const jateTransaction = jateDb.transaction('jate', 'readonly');
+
+  const jateStore = tx.objectStore('jate');
+
 
   // Leave the rest as-is
   const request = store.get(1);
