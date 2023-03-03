edac813fe2ecd5809d672ee538b84a733ed7e305
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-03-03 14:46:22 -0600

    Fixed vairable names in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,13 +14,13 @@ const initdb = async () =>
 
 export const putDb = async (content) => {
   // asyncronously await the opening of the database
-  const databaseOpener = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1);
 
   // create a variable for the transaction
-  const jateTransaction = jateDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // create a variable for the store
-  const jateStore = tx.objectStore('jate');
+  const store = tx.objectStore('jate');
 
   // perform the update
   const request = store.put({ id: 1, value: content });
@@ -31,11 +31,11 @@ export const putDb = async (content) => {
 
 export const getDb = async () => {
   
-  const databaseOpener = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1);
 
-  const jateTransaction = jateDb.transaction('jate', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
-  const jateStore = tx.objectStore('jate');
+  const store = tx.objectStore('jate');
 
   const request = store.get(1);
   const result = await request;
